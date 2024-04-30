ed13ef6496c96e75b584754448c92d808f727404
alexanderreiprich <alexander.reiprich@has-furtwangen.de>
2024-04-30 11:48:43 +0200

    feat: fitness function

--- Abgabe/dist/main.js
+++ Abgabe/dist/main.js
@@ -1,6 +1,7 @@
 "use strict";
-// import { hillclimber, rateCandidate, mutateCandidate, rateCandidateBalanced, mutateCandidateProbability } from "./classes/hillclimber";
 // hillclimber(rateCandidateBalanced, mutateCandidateProbability, 20, 1000);
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.calcFitness = void 0;
 function getId(_id) {
     return document.getElementById(_id);
 }
@@ -62,7 +63,47 @@ function simulateSwapping() {
 function main() {
     createSquares();
     visualizeSquares();
-    simulateSwapping();
+    console.log(calcFitness(squares));
+    // simulateSwapping();
+}
+function calcFitness(_squares) {
+    let squares = _squares;
+    let fitness = 0;
+    for (let i = 0; i < squares.length; i++) {
+        for (let k = 0; k < squares[i].length - 1; k++) {
+            let similarity = getSimilarity(squares[i][k], squares[i][k + 1]);
+            if (similarity) {
+                fitness += similarity;
+            }
+        }
+    }
+    return fitness;
+}
+exports.calcFitness = calcFitness;
+function hexToRGB(_hex) {
+    const match = _hex.match(/^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i);
+    if (!match) {
+        return null;
+    }
+    return [
+        parseInt(match[1], 16),
+        parseInt(match[2], 16),
+        parseInt(match[3], 16)
+    ];
+}
+function calcColorDistance(_rgb1, _rgb2) {
+    const rDiff = _rgb1[0] - _rgb2[0];
+    const gDiff = _rgb1[1] - _rgb2[1];
+    const bDiff = _rgb1[2] - _rgb2[2];
+    return Math.sqrt(rDiff * rDiff + gDiff * gDiff + bDiff * bDiff);
+}
+function getSimilarity(_hex1, _hex2) {
+    const rgb1 = hexToRGB(_hex1);
+    const rgb2 = hexToRGB(_hex2);
+    if (!rgb1 || !rgb2) {
+        return null;
+    }
+    return calcColorDistance(rgb1, rgb2);
 }
 main();
 //# sourceMappingURL=main.js.map
---
+++ Abgabe/src/classes/fitness.ts
@@ -0,0 +1,44 @@
+export function calcFitness(_squares: string[][]): number {
+  let squares = _squares;
+  let fitness: number = 0;
+  for (let i = 0; i < squares.length; i++) {
+    for (let k = 0; k < squares[i].length - 1; k++) {
+      let similarity = getSimilarity(squares[i][k], squares[i][k+1])
+      if(similarity) {
+        fitness += similarity;
+      }
+    }
+  }
+  return fitness; 
+}
+
+function hexToRGB(_hex: string): [number, number, number] | null {
+  const match = _hex.match(/^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i);
+  if (!match) {
+    return null;
+  }
+
+  return [
+    parseInt(match[1], 16),
+    parseInt(match[2], 16),
+    parseInt(match[3], 16)
+  ];
+}
+
+function calcColorDistance(_rgb1: [number, number, number], _rgb2: [number, number, number]): number {
+  const rDiff = _rgb1[0] - _rgb2[0];
+  const gDiff = _rgb1[1] - _rgb2[1];
+  const bDiff = _rgb1[2] - _rgb2[2];
+  return Math.sqrt(rDiff * rDiff + gDiff * gDiff + bDiff * bDiff);
+}
+
+function getSimilarity(_hex1: string, _hex2: string) {
+  const rgb1 = hexToRGB(_hex1);
+  const rgb2 = hexToRGB(_hex2);
+
+  if (!rgb1 || !rgb2) {
+    return null;
+  }
+
+  return calcColorDistance(rgb1, rgb2);
+}
