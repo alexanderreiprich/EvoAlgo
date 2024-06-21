dbef479e63a84f99ad567a70a0881e44e7049f2b
alexanderreiprich <al3xz0n3@gmail.com>
2024-06-21 12:53:20 +0200

    fix: folder structure

--- Übung 1/src/main.ts
+++
@@ -1,3 +0,0 @@
-import { hillclimber, rateCandidate, mutateCandidate, rateCandidateBalanced, mutateCandidateProbability } from "./classes/hillclimber";
-
-hillclimber(rateCandidateBalanced, mutateCandidateProbability, 20, 1000);
